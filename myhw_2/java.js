2c7d126eb86844fc595ca68bd355402c9f63a66d
cheayoons <wndkd280@daum.net>
2019-03-31 15:25:46 +0900

    cy

--- myhw_2/java.js
+++ myhw_2/java.js
@@ -2,9 +2,48 @@ function say() {
     alert("hello");
 }
 
-
+i = 0;
 function picture() {
-    top.down.document.pic.src = "test_2.jpg";
+    i = i + 1;
+    
+    if (i > 0) {
+        if (i%3==1) {
+            top.down.document.pic.src = "test_2.jpg"
+        }
+        if (i%3==2) {
+            top.down.document.pic.src = "test_3.jpg"
+        }
+        if (i%3==0) {
+            top.down.document.pic.src = "test_1.jpg"
+        }
+
+
+    }
+    
+}
+
+function picture_2() {
+    if (i == 0) {
+        i = 0;
+    }
+
+    if (i > 0) {
+        i = i - 1;
+    }
+
+    if (i > 0) {
+        if (i%3==1) {
+            top.down.document.pic.src = "test_2.jpg"
+        }
+        if (i%3==2) {
+            top.down.document.pic.src = "test_3.jpg"
+        }
+        if (i%3==0) {
+            top.down.document.pic.src = "test_1.jpg"
+        }
+
+
+    }
 
 
 }
